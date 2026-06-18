a3ae825e80af3168034b8eabea93a9c66f92bf5d
erayagency <eraylabs@gmail.com>
2026-06-18 21:55:44 +0300

    Performance: _headers caching, preconnect DNS, font-awesome optimize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,9 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
